798c92a10a3a021b4b06e97072f9d3d8195275fa
Hayden Roney <roneyhayden@gmail.com>
2024-02-12 11:09:34 -0600

    lab done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        returnArray = []
+        for arrayword in words:
+           if (sorted([letter for letter in self.word]) == sorted([letter for letter in arrayword])):
+               returnArray.append(arrayword)
+    
+        return returnArray
